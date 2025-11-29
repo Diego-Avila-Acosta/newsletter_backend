c179b07eea282a9a2517716793658385ea5a604a
Diego Avila <aviladiego2009@gmail.com>
2025-11-29 14:06:34 -0300

    Moved lib code to startup.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,3 @@
-use std::net::TcpListener;
-
-use actix_web::dev::Server;
-use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
-use sqlx::PgPool;
-
-use crate::routes::subscribe;
-
 pub mod configuration;
-mod routes;
-
-async fn healtch_check(req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok().finish()
-}
-
-pub fn run(listener: TcpListener, connection: PgPool) -> Result<Server, std::io::Error> {
-    let connection = web::Data::new(connection);
-
-    let server = HttpServer::new(move || {
-        App::new()
-            .route("/health_check", web::get().to(healtch_check))
-            .route("/subscriptions", web::post().to(subscribe))
-            .app_data(connection.clone())
-    })
-    .listen(listener)?
-    .run();
-
-    Ok(server)
-}
+pub mod routes;
+pub mod startup;
---
+++ src/startup.rs
@@ -0,0 +1,26 @@
+use std::net::TcpListener;
+
+use actix_web::dev::Server;
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
+use sqlx::PgPool;
+
+use crate::routes::subscribe;
+
+async fn healtch_check(req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+pub fn run(listener: TcpListener, connection: PgPool) -> Result<Server, std::io::Error> {
+    let connection = web::Data::new(connection);
+
+    let server = HttpServer::new(move || {
+        App::new()
+            .route("/health_check", web::get().to(healtch_check))
+            .route("/subscriptions", web::post().to(subscribe))
+            .app_data(connection.clone())
+    })
+    .listen(listener)?
+    .run();
+
+    Ok(server)
+}
