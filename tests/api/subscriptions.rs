4393a72781f3d4332cce2a79aa9e9a3ed2612ff0
Diego Avila <aviladiego2009@gmail.com>
2025-12-03 18:44:32 -0300

    Added new test to subscriptions

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -24,8 +24,29 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
     let test_cases = vec![
         ("name=diego", "missing the email"),
         ("email=diego20@gmail.com", "missing the name"),
-        ("name=diego&email=invalid-email", "invalid email"),
         ("", "missing both"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = app.post_subscription(invalid_body.into()).await;
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}",
+            error_message
+        );
+    }
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
+    let app = spawn_app().await.expect("Failed to spawn our app.");
+
+    let test_cases = vec![
+        ("name=&email=diego20@gmail.com", "empty name"),
+        ("name=diego&email=", "empty email"),
+        ("name=diego&email=invalid-email", "invalid email"),
     ];
 
     for (invalid_body, error_message) in test_cases {
