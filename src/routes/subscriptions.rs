de12501317ccd7f6893d90846522da931c497d5e
Diego Avila <aviladiego2009@gmail.com>
2025-12-04 11:39:46 -0300

    Insert query defaults to 'pending_confirmation' to status

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -64,7 +64,7 @@ pub async fn subscribe(
 pub async fn insert_subscriber(pool: &PgPool, form: &NewSubscriber) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"INSERT INTO subscriptions (id, email, name, subscribed_at, status)
-        VALUES ($1, $2, $3, $4, 'confirmed')
+        VALUES ($1, $2, $3, $4, 'pending_confirmation')
         "#,
         Uuid::new_v4(),
         form.email.as_ref(),
