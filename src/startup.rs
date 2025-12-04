2c7be8ae0e3c2ae017e8fb146d5fc0fe096e393c
Diego Avila <aviladiego2009@gmail.com>
2025-12-04 14:48:07 -0300

    Added subscriptions_confirm skeleton handler

--- src/startup.rs
+++ src/startup.rs
@@ -8,8 +8,7 @@ use tracing_actix_web::TracingLogger;
 
 use crate::configuration::Settings;
 use crate::email_client::EmailClient;
-use crate::routes::health_check;
-use crate::routes::subscribe;
+use crate::routes::*;
 
 pub struct Application {
     port: u16,
@@ -73,6 +72,7 @@ fn run(
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .route("/subscriptions/confirm", web::get().to(confirm))
             .app_data(connection.clone())
             .app_data(email_client.clone())
     })
