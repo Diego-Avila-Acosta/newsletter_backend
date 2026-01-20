9eeff59d4d4c984a720bf09067de17acbcd7bc3e
Diego Avila <aviladiego2009@gmail.com>
2026-01-20 16:22:03 -0300

    Added link a tag to /admin/password in admin dashboard

--- src/routes/admin/dashboard.rs
+++ src/routes/admin/dashboard.rs
@@ -28,6 +28,7 @@ pub async fn admin_dashboard(
     </head>
     <body>
         <p>Welcome {username}</p>
+        <p><a href="/admin/password">Change password</a></p>
     </body>
 </html>"#
         )))
