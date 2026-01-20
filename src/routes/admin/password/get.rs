26272d52b0fe753676629ae8421be9fe98a2f08b
Diego Avila <aviladiego2009@gmail.com>
2026-01-20 16:47:00 -0300

    Added check new_password and new_password_confirm, with sending flash message

--- src/routes/admin/password/get.rs
+++ src/routes/admin/password/get.rs
@@ -1,24 +1,37 @@
+use std::fmt::Write;
+
 use actix_web::{HttpResponse, http::header::ContentType};
+use actix_web_flash_messages::IncomingFlashMessages;
 
 use crate::{
     session_state::TypedSession,
     utils::{e500, see_other},
 };
 
-pub async fn change_password_form(session: TypedSession) -> Result<HttpResponse, actix_web::Error> {
+pub async fn change_password_form(
+    session: TypedSession,
+    flash_messages: IncomingFlashMessages,
+) -> Result<HttpResponse, actix_web::Error> {
     if session.get_user_id().map_err(e500)?.is_none() {
         return Ok(see_other("login"));
     }
 
-    Ok(HttpResponse::Ok().content_type(ContentType::html()).body(
-        r#"<!DOCTYPE html>
+    let mut msg_html = String::new();
+    for m in flash_messages.iter() {
+        writeln!(msg_html, "<p><i>{}</i></p>", m.content()).unwrap();
+    }
+
+    Ok(HttpResponse::Ok()
+        .content_type(ContentType::html())
+        .body(format!(
+            r#"<!DOCTYPE html>
 <html lang="e">
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
     <title>Change Password</title>
 </head>
 <body>
-
+    {msg_html}
     <form action="/admin/password" method="post">
         <label>Current password
             <input type="password" placeholder="Enter current password" name="current_password">
@@ -44,5 +57,5 @@ pub async fn change_password_form(session: TypedSession) -> Result<HttpResponse,
 
 
         "#,
-    ))
+        )))
 }
