e9fa56c977cbef8d6f13b4f5ad356124248c6020
Diego Avila <aviladiego2009@gmail.com>
2026-01-21 21:22:55 -0300

    Added new change_password & compute_password_hash functions

--- src/authentication.rs
+++ src/authentication.rs
@@ -1,9 +1,10 @@
-use anyhow::Context;
-use argon2::{Argon2, PasswordHash, PasswordVerifier};
+use anyhow::{Context, Ok};
+use argon2::PasswordHasher;
+use argon2::{Argon2, Params, PasswordHash, PasswordVerifier, password_hash::SaltString};
 use secrecy::{ExposeSecret, Secret};
 use sqlx::PgPool;
 
-use crate::telemetry::spawn_blocking_with_tracing;
+use crate::{domain::AdminPassword, telemetry::spawn_blocking_with_tracing};
 
 #[derive(thiserror::Error, Debug)]
 pub enum AuthError {
@@ -90,4 +91,44 @@ fn verify_password_hash(
         )
         .context("Invalid password.")
         .map_err(AuthError::InvalidCredentials)
+}
+
+#[tracing::instrument(name = "Change password", skip(password))]
+pub async fn change_password(
+    user_id: uuid::Uuid,
+    password: AdminPassword,
+    pool: &PgPool,
+) -> Result<(), anyhow::Error> {
+    let password_hash = spawn_blocking_with_tracing(move || compute_password_hash(password))
+        .await?
+        .context("Failed to hash password")?;
+
+    sqlx::query!(
+        r#"
+        UPDATE users
+        SET password_hash = $1
+        WHERE user_id = $2
+        "#,
+        password_hash.expose_secret(),
+        user_id
+    )
+    .execute(pool)
+    .await
+    .context("Failed to change user's password in the database.")?;
+
+    Ok(())
+}
+
+fn compute_password_hash(password: AdminPassword) -> Result<Secret<String>, anyhow::Error> {
+    let salt = SaltString::generate(&mut rand::thread_rng());
+
+    let password_hash = Argon2::new(
+        argon2::Algorithm::Argon2id,
+        argon2::Version::V0x13,
+        Params::new(15000, 2, 1, None).unwrap(),
+    )
+    .hash_password(password.as_ref().as_bytes(), &salt)?
+    .to_string();
+
+    Ok(Secret::new(password_hash))
 }
