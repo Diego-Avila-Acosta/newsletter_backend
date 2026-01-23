183d634e741b045066a656f5b0496b6aea9a7b0d
Diego Avila <aviladiego2009@gmail.com>
2026-01-23 14:10:20 -0300

    Moved old newsletters integration test to test/api/admin/newsletters.rs

--- tests/api/admin/newsletters.rs
+++ tests/api/admin/newsletters.rs
@@ -1,4 +1,9 @@
-use crate::helpers::{assert_is_redirect_to, spawn_app};
+use reqwest::StatusCode;
+use wiremock::Mock;
+use wiremock::ResponseTemplate;
+use wiremock::matchers::{method, path};
+
+use crate::helpers::{ConfirmationLinks, TestApp, assert_is_redirect_to, spawn_app};
 
 #[tokio::test]
 async fn must_be_logged_in_to_get_send_issue_form() {
@@ -12,7 +17,143 @@ async fn must_be_logged_in_to_get_send_issue_form() {
 async fn logged_user_can_get_send_issuer_form() {
     let app = spawn_app().await;
 
-    let html_page = app.get_change_password_html().await;
+    let user_credentials = serde_json::json!({
+        "username": &app.test_user.username,
+        "password": &app.test_user.password
+    });
 
+    let response = app.post_login(&user_credentials).await;
+    assert_is_redirect_to(&response, "/admin/dashboard");
+
+    let html_page = app.get_send_issue_html().await;
     assert!(html_page.contains("<p>Send new issue</p>"))
+}
+
+#[tokio::test]
+async fn must_be_logged_in_to_send_an_issue() {
+    let app = spawn_app().await;
+
+    let newsletter_request_body = serde_json::json!({
+        "title": "Newsletter title",
+        "text_content": "Newsletter body as plain text",
+        "html_content": "<p>Newsletter body as HTML</p>",
+    });
+
+    let response = app.post_send_issue(newsletter_request_body).await;
+    assert_is_redirect_to(&response, "/login");
+}
+
+#[tokio::test]
+async fn newsletters_are_not_delivered_to_unconfirmed_subscribers() {
+    let app = spawn_app().await;
+    create_unconfirmed_subscriber(&app).await;
+
+    app.login_user().await;
+
+    let _mock_guard = Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(0)
+        .mount(&app.email_server)
+        .await;
+
+    let newsletter_request_body = serde_json::json!({
+        "title": "Newsletter title",
+        "text_content": "Newsletter body as plain text",
+        "html_content": "<p>Newsletter body as HTML</p>",
+    });
+
+    let response = app.post_send_issue(newsletter_request_body).await;
+
+    assert_eq!(response.status().as_u16(), StatusCode::ACCEPTED);
+}
+
+#[tokio::test]
+async fn newsletters_are_delivered_to_confirmed_subscribers() {
+    let app = spawn_app().await;
+    create_confirmed_subscriber(&app).await;
+
+    app.login_user().await;
+
+    let _mock_guard = Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    let newsletter_request_body = serde_json::json!({
+        "title": "Newsletter title",
+        "text_content": "Newsletter body as plain text",
+        "html_content": "<p>Newsletter body as HTML</p>"
+    });
+
+    let response = app.post_send_issue(newsletter_request_body).await;
+
+    assert_eq!(response.status().as_u16(), StatusCode::ACCEPTED);
+}
+
+async fn create_unconfirmed_subscriber(app: &TestApp) -> ConfirmationLinks {
+    let body = "name=diego&email=diego20@gmail.com";
+
+    let _mock_guard = Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount_as_scoped(&app.email_server)
+        .await;
+
+    app.post_subscription(body.into())
+        .await
+        .error_for_status()
+        .unwrap();
+
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+
+    app.get_confirmation_links(&email_request)
+}
+
+async fn create_confirmed_subscriber(app: &TestApp) {
+    let confirmation_link = create_unconfirmed_subscriber(app).await;
+
+    reqwest::get(confirmation_link.plain_text)
+        .await
+        .unwrap()
+        .error_for_status()
+        .unwrap();
+}
+
+#[tokio::test]
+async fn newsletters_return_400_for_invalid_data() {
+    let app = spawn_app().await;
+    create_unconfirmed_subscriber(&app).await;
+
+    app.login_user().await;
+
+    let test_cases = [
+        (
+            serde_json::json!({
+                "title": "Newsletter title",
+            }),
+            "missing content",
+        ),
+        (
+            serde_json::json!({
+                "text_content": "Newsletter body as plain text",
+                "html_content": "<p>Newsletter body as HTML</p>"
+            }),
+            "missing title",
+        ),
+    ];
+
+    for (invalid_json, message) in test_cases {
+        let response = app.post_send_issue(invalid_json).await;
+
+        assert_eq!(
+            response.status().as_u16(),
+            400,
+            "The API did not fail with 400 Bad Request when te payload was {}",
+            message
+        );
+    }
 }
