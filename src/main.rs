dfb19049127361716c474d2e3646b8d948319a80
Diego Avila <aviladiego2009@gmail.com>
2026-01-30 12:40:15 -0300

    Added call init_prometheus_exporter in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use newsletter_backend::configuration::get_configuration;
+use newsletter_backend::metrics::init_prometheus_exporter;
 use newsletter_backend::startup::Application;
 use newsletter_backend::telemetry::{get_subscriber, init_subscriber};
 
@@ -6,6 +7,8 @@ use newsletter_backend::telemetry::{get_subscriber, init_subscriber};
 async fn main() -> anyhow::Result<()> {
     let subscriber = get_subscriber("newsletter_backend".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
+
+    init_prometheus_exporter();
 
     let configuration = get_configuration().expect("Error reading initial configuration");
 
