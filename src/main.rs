e7743b059c74fc18c5c3de302967f9bd59d7bb0b
Diego Avila <aviladiego2009@gmail.com>
2026-01-30 14:11:55 -0300

    Modified main entry point to shutdown provider with the app

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use newsletter_backend::configuration::get_configuration;
 use newsletter_backend::metrics::init_prometheus_exporter;
 use newsletter_backend::startup::Application;
-use newsletter_backend::telemetry::{get_subscriber, init_subscriber};
+use newsletter_backend::telemetry::{get_opentelemetry_parts, get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let subscriber = get_subscriber("newsletter_backend".into(), "info".into(), std::io::stdout);
+    let configuration = get_configuration().expect("Error reading initial configuration");
+
+    let (tracer, provider) = get_opentelemetry_parts(&configuration.tracer.export_endpoint);
+
+    let subscriber = get_subscriber(tracer, "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
     init_prometheus_exporter();
@@ -13,5 +17,11 @@ async fn main() -> anyhow::Result<()> {
     let configuration = get_configuration().expect("Error reading initial configuration");
 
     let server = Application::build(configuration).await?;
-    server.run_until_stopped().await
+    server.run_until_stopped().await?;
+
+    provider
+        .shutdown()
+        .expect("Failed to close tracer provider");
+
+    Ok(())
 }
